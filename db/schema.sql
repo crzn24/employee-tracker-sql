11d68e2880983506228e893b7fabd067c8c486d3
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-12-14 19:36:21 -0800

    Starts work on schema.sql file for tables

---
+++ db/schema.sql
@@ -0,0 +1,24 @@
+-- When developing, it's best to do drop before creating --
+DROP DATABASE IF EXISTS employees_db;
+-- Creates "employees_db" database --
+CREATE DATABASE employees_db;
+
+-- Makes it so all of the following code will affect employees_db --
+USE employees_db;
+
+
+
+-- Creates the table "department" within employees_db --
+CREATE TABLE department (
+
+);
+
+-- Creates the table "role" within employees_db --
+CREATE TABLE role (
+
+);
+
+-- Creates the table "employee" within employees_db --
+CREATE TABLE employee (
+
+);
