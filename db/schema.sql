a76d5219be65c63c927b233b1ba999f1a263c151
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-12-14 20:52:45 -0800

    Adds in missing PRIMARY KEY for all tables to allow auto increment

--- db/schema.sql
+++ db/schema.sql
@@ -10,15 +10,16 @@ USE employees_db;
 
 -- Creates the table "department" within employees_db --
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT, -- Creates numeric column that automatically increments its default value when creating new rows --
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, -- Creates numeric column that automatically increments its default value when creating new rows --
     name VARCHAR(30) NOT NULL, -- Makes a string column called "name" which cannot contain null --
 );
 
 -- Creates the table "role" within employees_db --
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
+    department_id INT,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
     ON DELETE SET NULL
@@ -26,9 +27,11 @@ CREATE TABLE role (
 
 -- Creates the table "employee" within employees_db --
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
     FOREIGN KEY (role_id)
     REFERENCES role(id)
     ON DELETE SET NULL
