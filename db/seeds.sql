4f9b679060d24c2d88228f368b91a5b97029dbc6
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-12-17 20:05:06 -0800

    Adds in employees for seed

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,7 +6,8 @@ VALUES
     (Legal),
     (Engineering),
     (Human Resources),
-    (Accounting/Finance);
+    (Accounting/Finance),
+    (Executive Department);
 
 -- other departments in a business/company
 -- production
@@ -26,13 +27,19 @@ VALUES
     (HR Manager, 100000, 4),
     (HR, 60000, 4),
     (Account Manager, 160000, 5),
-    (Accountant, 125000, 5);
+    (Accountant, 125000, 5),
+    (CEO, 300000, 6);
 
 
-INSERT INTO employee (name)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
-    (),
-    (),
-    (),
-    (),
-    ();
+    (Admiral, Ackbar, 1, NULL),
+    (Lando Calrissian, 2, 1),
+    (Leia, Organa, 3, NULL),
+    (Mace, Windu, 4, 3),
+    (Han, Solo, 5, NULL),
+    (Sabine, Wren, 6, 5),
+    (Poe Dameron, 6, 5),
+    (ObiWan, Kenobi, 7, NULL),
+    (Ahsoka, Tano, 8, 7),
+    (Luke, Skywalker, 11, NULL);
