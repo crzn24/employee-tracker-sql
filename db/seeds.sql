84244af980a0ed8ae5c8e7ec548b72397da3d1b8
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-12-17 19:27:11 -0800

    Adds seeds for department values

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,20 @@
 USE employees_db;
 
+INSERT INTO department (name)
+VALUES 
+    (Management),
+    (Marketing/Sales),
+    (Legal),
+    (Engineering),
+    (Human Resources),
+    (Accounting/Finance);
+
+-- department
+-- role
+-- employee
 
 
+-- other departments in a business/company
+-- production, research and development 
+-- IT
+-- Operations.
