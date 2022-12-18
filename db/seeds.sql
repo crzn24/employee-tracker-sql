887b35dc8fdc751c5bcd1339d45f0b9b6d6af019
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-12-17 19:37:36 -0800

    Adds seeds for role

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,19 +2,37 @@ USE employees_db;
 
 INSERT INTO department (name)
 VALUES 
-    (Management),
     (Marketing/Sales),
     (Legal),
     (Engineering),
     (Human Resources),
     (Accounting/Finance);
 
--- department
--- role
--- employee
-
-
 -- other departments in a business/company
--- production, research and development 
+-- production
+-- research and development 
 -- IT
--- Operations.
+-- Operations
+-- Management
+
+INSERT INTO role (title, salary, department_id)
+VALUES 
+    (Sales Lead, 100000, 1),
+    (Salesperson, 80000, 1),
+    (Legal Team Lead, 250000, 2),
+    (Lawyer, 190000, 2),
+    (Lead Engineer, 150000, 3),
+    (Software Engineer, 120000, 3),
+    (HR Manager, 100000, 4),
+    (HR, 60000, 4),
+    (Account Manager, 160000, 5),
+    (Accountant, 125000, 5);
+
+
+INSERT INTO employee (name)
+VALUES 
+    (),
+    (),
+    (),
+    (),
+    ();
