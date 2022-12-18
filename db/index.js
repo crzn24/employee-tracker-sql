dfddd8ac866a618cc0aa0d7933bf1b402d8c6e7f
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-12-17 18:34:06 -0800

    Adds an index.js file to db folder for connection to node based on activity 11

---
+++ db/index.js
@@ -0,0 +1,22 @@
+// Import and require mysql2
+const mysql = require("mysql2");
+
+// Connect to database
+////// this takes place of opening mysql so you don't have to put it in the CLI
+const db = mysql.createConnection(
+  {
+    host: "localhost", // MySQL username,
+    user: "root", // MySQL password
+    password: "Password!", // this is where you put your password
+    database: "employees_db",
+  },
+  console.log(`Connected to the employees_db database.`)
+);
+
+// Query database
+db.query("SELECT * FROM employees", function (err, results) {
+  console.log(results);
+//   db.end(); // added this to close connection (for homework, don't end here, only close after inquirer questions)
+});
+/////// this is an asynchronus function because query could take different time to complete. the function part is a callback handler
+//// need to close sql connection
