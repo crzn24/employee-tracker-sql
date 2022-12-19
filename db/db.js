ddd5c502745ae7de225194a9ee367204ec00c87b
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-12-19 13:28:01 -0800

    Adds methods into class

--- db/db.js
+++ db/db.js
@@ -1,29 +1,32 @@
 // Separate file for performing specific SQL queries that I'll need to use. Constructor function or class helpful to organize
 
-const db = require("./sqlconnect")
+const db = require("./sqlconnect");
 
+class dbEmployees {
 
-
-constructor(db) {
+  // connect to mysql  
+  constructor(db) {
     this.db = db;
+  }
+
+  // method to show all employees
+  showAllEmployees() {
+    return this.db;
+  }
+
+  // method to add employee
+
+  // method to update employee role
+
+  // method to show all roles
+
+  // method to add role
+
+  // method to show all departments
+
+  // method to add department
+
+
+
+  
 }
-
-// method to show all employees
-showAllEmployees() {
-    return this.db.pr
-}
-
-// method to add employee
-
-// method to update employee role
-
-// method to show all roles
-
-// method to add role
-
-// method to show all departments
-
-// method to add department
-
-
-
