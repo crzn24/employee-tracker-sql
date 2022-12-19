b2df26851b0572d2c3ce5a0db9829098349f4043
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-12-19 15:04:20 -0800

    starts first method and adds comments

--- db/db.js
+++ db/db.js
@@ -4,14 +4,16 @@ const db = require("./sqlconnect");
 
 class dbEmployees {
 
-  // connect to mysql  
+  // first thing is create a reference to the connection on the class in case we need it later - connect to mysql 
   constructor(db) {
     this.db = db;
   }
 
-  // method to show all employees
-  showAllEmployees() {
-    return this.db;
+  // method to show all employees - This will find all employees, join with roles and departments to display their roles, salaries, departments, and managers
+  findAllEmployees() {
+    return this.db.promise().query(
+        "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
+    );
   }
 
   // method to add employee
@@ -28,5 +30,5 @@ class dbEmployees {
 
 
 
-  
+  // Make sure to use .promise() on each connection inside each method
 }
