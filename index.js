26b5ae58743da9b94aee0e0ee78dfd3bf5ad7a41
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-12-20 12:51:36 -0800

    Finishes function for adding a new role to db

--- index.js
+++ index.js
@@ -152,8 +152,36 @@ function viewAllRoles() {
 
 // Function to Add Role
 function addNewRole() {
+    db.findAllDepartments()
+    .then(([rows]) => {
+        let departments = rows;
+        const departmentChoices = departments.map(({id, name}) =>({
+            name: name,
+            value: id
+        }))
+        
+        inquirer.prompt([
+            {
+                name: "title",
+                message: "What is the name of the role?"
+            },
+            {
+                name: "salary",
+                message: "What is the salary of the role?"
+            },
+            {
+                type: "list",
+                name: "department_id",
+                message: "Which department does the role belong to?",
+                choices: departmentChoices
+            }
+        ]) .then(role => {
+                db.addRole(role)
+                .then(() => console.log(`Added ${role.title} to the database`))
+                .then(() => askQuestions())
+        })
 
-
+    })
 
 }
 
