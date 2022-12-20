8902a1cc43cad61a9557b6c15da4e0eea493e34e
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-12-20 12:35:28 -0800

    Finishes the function to addnewemployee with prompts and updates

--- index.js
+++ index.js
@@ -36,16 +36,22 @@ function askQuestions() {
                 viewAllEmployees();
                 break;
             case ("Add Employee"):
-                addEmployee();
+                addNewEmployee();
                 break;
+            // case (""):
+            //     ();
+            //     break;
             case ("View All Roles"):
                 viewAllRoles();
                 break;
-            case ("Add Department"):
-                addNewDepartment();
-                break;
+            // case (""):
+            //     ();
+            //     break;
             case ("View All Departments"):
                 viewAllDepartments();
+                break; 
+            case ("Add Department"):
+                addNewDepartment();
                 break;
             default:
                 quit();
@@ -68,7 +74,73 @@ function viewAllEmployees() {
 } 
 
 // Function to Add Employee
+function addNewEmployee() {
+    inquirer.prompt([
+       {
+            name:"first_name",
+            message: "What is the employee's first name?"
+       },
+       {
+            name:"last_name",
+            message: "What is the employee's last name?"
+       }
+    ]) .then(res => {
+            let firstName = res.first_name;
+            let lastName = res.last_name;
+        db.findAllRoles()
+        .then(([rows]) => {
+            let roles = rows;
+            const roleChoices = roles.map(({id, title}) => ({
+                name: title,
+                value: id
+            }));
+            inquirer.prompt({
+                type: "list",
+                name: "roleId",
+                message: "What is the employee's role?",
+                choices: roleChoices
+            })
+            .then(res => {
+                let roleId = res.roleId;
 
+                db.findAllEmployees()
+                .then(([rows]) => {
+                    let employees = rows;
+                    const managerChoices = employees.map(({id, first_name, last_name}) => ({
+                        name: `${first_name} ${last_name}`,
+                        value: id
+                    }));
+
+                    managerChoices.unshift({name: "None",value: null});
+
+                    inquirer.prompt({
+                        type: "list",
+                        name: "managerId",
+                        message: "Who is the employee's manager?",
+                        choices: managerChoices
+                    }) .then(res => {
+                        let employee = {
+                            manager_id: res.managerId,
+                            role_id: roleId,
+                            first_name: firstName,
+                            last_name: lastName
+                        }
+
+                        db.addEmployee(employee);
+                    }) .then(() => console.log(
+                        `Added ${firstName} ${lastName} to the database`
+                    )) .then(() => askQuestions())
+
+                })
+            })
+        })
+    });
+
+
+    
+    
+    // addEmployee
+}
 
 // Function to Update Employee Role
 
