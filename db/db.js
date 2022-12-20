b2ecc1b68475e3053573e73522059b9b7c89b1aa
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-12-19 18:12:22 -0800

    Changes employees in method to employee singular

--- db/db.js
+++ db/db.js
@@ -1,18 +1,18 @@
 // Separate file for performing specific SQL queries that I'll need to use. Constructor function or class helpful to organize
 
-const db = require("./sqlconnect");
+const connection = require("./sqlconnect");
 
-class dbEmployees {
+class EmployeesDB {
 
   // first thing is create a reference to the connection on the class in case we need it later - connect to mysql 
-  constructor(db) {
-    this.db = db;
+  constructor(connection) {
+    this.connection = connection;
   }
 
   // method to show all employees - This will find all employees, join with roles and departments to display their roles, salaries, departments, and managers
   findAllEmployees() {
-    return this.db.promise().query(
-        "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
+    return this.connection.promise().query(
+        "SELECT * FROM employee;"
     );
   }
 
@@ -33,4 +33,4 @@ class dbEmployees {
   // Make sure to use .promise() on each connection inside each method
 }
 
-module.exports = db;
+module.exports = new EmployeesDB(connection);
